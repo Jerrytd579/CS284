93c34184c01f0d22ea29196fe75921c0c7960540
Jerry Cheng <jerrytd579@gmail.com>
2019-01-25 11:03:36 -0500

    1/25/2019

--- src/Rectangle.java
+++ src/Rectangle.java
@@ -1,6 +1,14 @@
 public class Rectangle {
+
+    // Data Fields
     private int width;
     private int height;
+
+    // Constructor
+    Rectangle(int width, int height) {
+        this.width = width;
+        this.height = height;
+    }
 
     // Methods
     public int getHeight() {
@@ -20,11 +28,11 @@ public class Rectangle {
     }
 
     public static void main(String[] args) {
-        Rectangle r1 = new Rectangle();
-        Rectangle r2 = new Rectangle();
+        Rectangle r1 = new Rectangle(4,5);
+        Rectangle r2 = new Rectangle(7,9);
         r1.setWidth(5);
         r2.setWidth(4);
-        System.out.println("Rectangle r1 has width" + r1);
-        System.out.println("Rectangle r1 was width" + r2);
+        System.out.println("Rectangle r1 has width " + r1.getWidth());
+        System.out.println("Rectangle r2 was height " + r2.getHeight());
     }
 }
