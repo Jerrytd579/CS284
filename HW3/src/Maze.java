7c278e9dc93b4ae6ceedd0b27c1736be9fd8284f
Jerry Cheng <jerrytd579@gmail.com>
2019-04-04 18:28:59 -0400

    4/4/2019

--- HW3/src/Maze.java
+++ HW3/src/Maze.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 /**
  * Class that solves maze problems with backtracking.
  * @author Koffman and Wolfgang
@@ -29,10 +31,39 @@ public class Maze implements GridColors {
      *         otherwise, false
      */
     public boolean findMazePath(int x, int y) {
-        // COMPLETE HERE FOR PROBLEM 1
+        // Cell is either out of bounds or is on barrier or dead end
+        if (x < 0 || y < 0 || x >= maze.getNCols() || y >= maze.getNRows()
+                || maze.getColor(x, y) != NON_BACKGROUND) {
+            return false;
+        }
+        // Cell is on path and is maze exit
+        else if (x == maze.getNCols() - 1 && y == maze.getNRows() - 1) {
+            maze.recolor(x, y, PATH);
+            return true;
+        }
+        else {
+            // Attempts to find a path from each neighbor
+            maze.recolor(x, y, PATH);
+            if (findMazePath(x - 1, y) || findMazePath(x + 1, y) || findMazePath(x, y - 1) ||
+            findMazePath(x, y + 1)) {
+                return true;
+            }
+            else {
+                maze.recolor(x, y, TEMPORARY);
+                return false;
+            }
+        }
     }
 
-    // ADD METHOD FOR PROBLEM 2 HERE
+    /**
+     * Modifies the solution of problem 1 so that a list of all the solutions to the maze is returned
+     * @param x the x-coordinate of the current point
+     * @param y the y-corrdinate of the current point
+     * @return a list of all the solutions to the maze
+     */
+    public ArrayList<ArrayList<PairInt>> findAllMazePaths(int x, int y) {
+
+    }
     
     // ADD METHOD FOR PROBLEM 3 HERE
     
--- HW3/src/PairInt.java
+++ HW3/src/PairInt.java
@@ -1,2 +1,46 @@
 public class PairInt {
+
+    private int x;
+    private int y;
+
+
+    public PairInt(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setX() {
+        this.x = x;
+    }
+
+    public void setY() {
+        this.y = y;
+    }
+
+    public boolean equals(Object p) {
+        PairInt obj = (PairInt) p;
+        return x == obj.getX() && y == obj.getY();
+    }
+
+    public String toString() {
+        StringBuilder result = new StringBuilder();
+        result.append("(");
+        result.append(x);
+        result.append(", ");
+        result.append(y);
+        result.append(")");
+        return result.toString();
+    }
+
+    public PairInt copy() {
+        return new PairInt(this.x, this.y);
+    }
 }
